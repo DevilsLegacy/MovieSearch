735a74f94b4c8cbd755b1f04735d81a54cae119e
Yash Gupta <guptayash3@gmail.com>
2018-04-03 00:20:36 +0530

    updated top rated movie query to fetch any page.

--- src/main/java/com/devilslegacy/moviesearch/rest/ApiInterface.java
+++ src/main/java/com/devilslegacy/moviesearch/rest/ApiInterface.java
@@ -2,10 +2,13 @@ package com.devilslegacy.moviesearch.rest;
 
 import com.devilslegacy.moviesearch.model.MoviesResponse;
 
+import java.util.Map;
+
 import retrofit2.Call;
 import retrofit2.http.GET;
 import retrofit2.http.Path;
 import retrofit2.http.Query;
+import retrofit2.http.QueryMap;
 
 /**
  * Created by devil on 3/5/2018.
@@ -14,7 +17,7 @@ import retrofit2.http.Query;
 public interface ApiInterface {
 
     @GET("movie/top_rated")
-    Call<MoviesResponse> getTopRatedMovies(@Query("api_key") String apiKey);
+    Call<MoviesResponse> getTopRatedMovies(@QueryMap Map<String, String> options);
 
     @GET("movie/{id}")
     Call<MoviesResponse> getMovieDetails(@Path("id") int id, @Query("api_key") String apiKey);
